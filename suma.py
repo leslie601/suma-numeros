f262fc5d39daebe2362da7e1d7551e2eb25d6d2d
leslie601 <lesliepatricialf@isb.edu.mx>
2024-08-28 08:16:54 -0600

    Se sube el archivo

---
+++ suma.py
@@ -0,0 +1,11 @@
+a=int(input("ingresa el primer numero: "))
+b=int(input("ingresa el segundo numero; "))
+
+
+#suma de los numeros
+
+suma=a+b
+
+#mostrar los resultados
+
+print("la suma es ",suma)
